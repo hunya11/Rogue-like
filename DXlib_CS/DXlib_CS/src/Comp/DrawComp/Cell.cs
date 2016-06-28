2f4d4261fc94893fcc35bc134730fe4e541b654b
hunya11 <shin5626@gmail.com>
2016-06-28 18:22:38 +0900

    Add Cell.cs

---
+++ DXlib_CS/DXlib_CS/src/Comp/DrawComp/Cell.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DXlib_CS.src.Comp.DrawComp.Image;
+
+namespace DXlib_CS.src.Comp.DrawComp.Object {
+    class Cell : DrawableComponent {
+
+        private ImageObject onBlock;
+        private ImageObject offBLock;
+
+        public double cellImageSizeX {
+            get {
+                return this.onBlock.SizeX;
+            }
+        }
+
+        public double cellImageSizeY {
+            get {
+                return this.onBlock.SizeY;
+            }
+        }
+
+
+        public Cell(double posX , double posY , ImageObject on , ImageObject off) {
+
+            this.onBlock = on;
+            this.offBLock = off;
+        }
+
+        public override void Init() {
+        }
+
+
+        public override void UpData() {
+        }
+
+        public override void Draw() {
+        }
+
+    }
+
+
+}
